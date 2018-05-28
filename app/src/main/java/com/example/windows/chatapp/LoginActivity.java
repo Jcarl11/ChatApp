076f5519266be623ff97e92bb0f8960d37f2183d
Joey Francisco <joeycarlofrancisco@gmail.com>
2018-05-29 00:08:51 +0800

    Added a basic functionality in login(No validation)

--- app/src/main/java/com/example/windows/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/windows/chatapp/LoginActivity.java
@@ -1,15 +1,65 @@
 package com.example.windows.chatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends Activity
 {
-
+    private TextInputEditText email,password;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.ET_EMAIL_LOGIN);
+        password = findViewById(R.id.ET_PASSWORD_LOGIN);
+    }
+
+    public void submitClicked(View view)
+    {
+        login(email.getText().toString().trim(), password.getText().toString().trim());
+    }
+    public void login(String user_email, String user_password)
+    {
+        mAuth.signInWithEmailAndPassword(user_email, user_password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            nextPage(MainActivity.class);
+                        }
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void nextPage(Class<?> destination)
+    {
+        Intent next = new Intent(LoginActivity.this, destination);
+        startActivity(next);
+        finish();
     }
 }
